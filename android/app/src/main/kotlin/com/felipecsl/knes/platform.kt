e1a34f00dbc292600c257e3ce4514ca84e3611d4
Felipe Lima <felipe.lima@gmail.com>
2018-09-28 22:23:20 -0700

    Faster array copt

--- android/app/src/main/kotlin/com/felipecsl/knes/platform.kt
+++ android/app/src/main/kotlin/com/felipecsl/knes/platform.kt
@@ -2,6 +2,7 @@ package com.felipecsl.knes
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import java.util.*
 
 actual fun currentTimeMs(): Long {
   return System.currentTimeMillis()
@@ -14,27 +15,12 @@ actual class AudioSink {
 
   @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
   actual fun write(value: Float) {
-    synchronized(lock) {
-      buffer[pos++] = value
-    }
+    buffer[pos++] = value
   }
 
   actual fun drain(): FloatArray {
-    synchronized(lock) {
-      val out = FloatArray(pos)
-      for (i in 0 until pos) {
-        out[i] = buffer[i]
-        buffer[i] = 0F
-      }
-      for (i in pos until bufferSize) {
-        buffer[i] = 0F
-      }
-      pos = 0
-      return out
-    }
-  }
-
-  companion object {
-    private val lock = Object()
+    val out = Arrays.copyOf(buffer, pos)
+    pos = 0
+    return out
   }
 }

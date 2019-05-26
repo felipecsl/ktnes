cb09be36652991cca79c4284407ad3f328012d09
Fernando Raviola <fernandoraviola@gmail.com>
2019-05-26 18:01:50 +1000

    Avoid unnecessary work while updating the texture

--- android/src/main/kotlin/com/felipecsl/knes/GLSprite.kt
+++ android/src/main/kotlin/com/felipecsl/knes/GLSprite.kt
@@ -10,6 +10,8 @@ import java.nio.IntBuffer
 class GLSprite {
   private var context: RenderContext? = null
   private var texture: Int? = null
+  private var initialized = false
+
   var director: Director? = null
 
   data class RenderContext(
@@ -68,33 +70,34 @@ class GLSprite {
     }
   }
 
-  private fun createTexture(image: IntArray): IntBuffer {
+  private fun updateTexture(image: IntArray) {
     val buffer = IntBuffer.wrap(image)
     glBindTexture(GL_TEXTURE_2D, texture!!)
-    glTexImage2D(GL_TEXTURE_2D, 0, GLES11Ext.GL_BGRA, IMG_WIDTH, IMG_HEIGHT, 0,
-        GLES11Ext.GL_BGRA, GL_UNSIGNED_BYTE, buffer)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
-    // Use our shader program
-    val context = context!!
-    glUseProgram(context.shaderProgram)
-    // Disable blending
-    glDisable(GL_BLEND)
-    // Set the vertex attributes
-    glVertexAttribPointer(context.texCoordHandle, 2, GL_FLOAT, false, 0, context.texVertices)
-    glEnableVertexAttribArray(context.texCoordHandle)
-    glVertexAttribPointer(context.posCoordHandle, 2, GL_FLOAT, false, 0, context.posVertices)
-    glEnableVertexAttribArray(context.posCoordHandle)
-    return buffer
-  }
+    if (!initialized) {
+      initialized = true
+      glTexImage2D(GL_TEXTURE_2D, 0, GLES11Ext.GL_BGRA, IMG_WIDTH, IMG_HEIGHT, 0,
+              GLES11Ext.GL_BGRA, GL_UNSIGNED_BYTE, buffer)
 
-  private fun updateTexture(image: IntArray) {
-    val buffer = createTexture(image)
-    glBindTexture(GL_TEXTURE_2D, texture!!)
-    glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, IMG_WIDTH, IMG_HEIGHT,
-        GLES11Ext.GL_BGRA, GL_UNSIGNED_BYTE, buffer)
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
+
+      // Use our shader program
+      val context = context!!
+      glUseProgram(context.shaderProgram)
+      // Disable blending
+      glDisable(GL_BLEND)
+      // Set the vertex attributes
+      glVertexAttribPointer(context.texCoordHandle, 2, GL_FLOAT, false, 0, context.texVertices)
+      glEnableVertexAttribArray(context.texCoordHandle)
+      glVertexAttribPointer(context.posCoordHandle, 2, GL_FLOAT, false, 0, context.posVertices)
+      glEnableVertexAttribArray(context.posCoordHandle)
+    } else {
+      glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, IMG_WIDTH, IMG_HEIGHT,
+              GLES11Ext.GL_BGRA, GL_UNSIGNED_BYTE, buffer)
+    }
+
     // Draw!
     glDrawArrays(GL_TRIANGLE_STRIP, 0, 4)
   }
